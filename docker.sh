0de05ffd6e4d68397cc4f7ec1cb6f242c03eb104
Payam <cpcpayam@gmail.com>
2023-11-24 13:41:38 +1000

    commands

--- docker.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -it -v ~/.tb-gateway/logs:/thingsboard_gateway/logs -v ~/.tb-gateway/extensions:/thingsboard_gateway/extensions -v ~/.tb-gateway/config:/thingsboard_gateway/config -p 8053:1883 --name tbGateway8322983184 -e host=83.229.83.184 -e port=1883 -e accessToken=aQSzwJBXbF1tBXxksIaz --restart always thingsboard/tb-gateway
