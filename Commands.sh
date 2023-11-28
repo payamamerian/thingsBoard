f3a3b4ef8f2359aa3c124e34dea58ad0b739baa1
Payam Amerian <payamamerian@gmail.com>
2023-11-29 07:32:36 +1000

    dummy

--- Commands.sh
+++ Commands.sh
@@ -1,4 +1,5 @@
-sudo docker run -it -v ~/.tb-gateway/logs:/thingsboard_gateway/logs -v ~/.tb-gateway/extensions:/thingsboard_gateway/extensions -v ~/.tb-gateway/config:/thingsboard_gateway/config -p 8053:1883 --name tbGateway8322983184 -e host=83.229.83.184 -e port=1883 -e accessToken=aQSzwJBXbF1tBXxksIaz --restart always thingsboard/tb-gateway
+sudo docker run -it -v ~/.tb-gateway/logs:/thingsboard_gateway/logs -v ~/.tb-gateway/extensions:/thingsboard_gateway/extensions -v ~/.tb-gateway/config:/thingsboard_gateway/config -p 8053:8053 --name tbGateway8322983184 -e host=83.229.83.184 -e port=1883 -e accessToken=qSG3MQNnHbzOGsZmL0cX --restart always thingsboard/tb-gateway
+
 
 sudo docker logs ffcb8034c7cd
 
