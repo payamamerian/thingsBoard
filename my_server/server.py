eb1c2d8a1c2fbe7be34001b9890757dbfb049d92
Payam Amerian <payamamerian@gmail.com>
2023-12-17 10:00:47 +1000

    update

--- my_server/server.py
+++ my_server/server.py
@@ -1,3 +1,4 @@
+import socket  # Add this line
 import subprocess
 import json
 from df703 import DF703  # Importing the DF703 class
@@ -5,6 +6,7 @@ from daemon import DaemonContext
 from sys import stdout, stderr
 
 def create_tcp_server():
+    server_socket = None  # Define server_socket here
     while True:  # Outer loop to restart the server on failure
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,18 +29,10 @@ def create_tcp_server():
                     # Assuming DF703.parse_data_DF703 can handle binary data directly
                     decoded_data = DF703.parse_data_DF703(data)
                     
-                    # Convert the decoded data to JSON string if it's a dictionary
-                    if isinstance(decoded_data, dict):
-                        json_data = json.dumps(decoded_data)
-                    else:
-                        json_data = str(decoded_data)
-
-                    curl_command = f"curl -v -X POST http://185.167.96.79:8080/api/v1/l9ollIobraqrDkjy4cEL/telemetry --header 'Content-Type:application/json' --data '{json_data}'"
-                    subprocess.run(curl_command, shell=True, check=True)
-
         except Exception as e:
             print(f"An error occurred: {e}")
-            server_socket.close()  # Close the server socket before restarting
+            if server_socket:  # Check if server_socket is not None before closing
+                server_socket.close()  # Close the server socket before restarting
 
 if __name__ == '__main__':
     with DaemonContext(stdout=stdout, stderr=stderr):
