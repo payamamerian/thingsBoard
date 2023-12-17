ab4cfd680a8967574f7561d6f775bebf8905ee28
Payam Amerian <payamamerian@gmail.com>
2023-12-17 11:40:59 +1000

    server

--- my_server/server.py
+++ my_server/server.py
@@ -1,26 +1,30 @@
-import socket  # Add this line
+import logging
+import socket
 import subprocess
 import json
 import time
-from df703 import DF703  # Importing the DF703 class
+from df703 import DF703
 from daemon import DaemonContext
 from sys import stdout, stderr
 
+# Set up logging
+logging.basicConfig(filename='server.log', level=logging.DEBUG)
+
 def create_tcp_server():
-    server_socket = None  # Define server_socket here
-    while True:  # Outer loop to restart the server on failure
+    server_socket = None
+    while True:
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Add this line
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             host = '185.167.96.79'
             port = 8053
             server_socket.bind((host, port))
             server_socket.listen(5)
-            print("Server listening on", host, ":", port)
+            logging.info("Server listening on %s : %s", host, port)
 
             while True:
                 client_socket, addr = server_socket.accept()
-                print('Got a connection from', addr)
+                logging.info('Got a connection from %s', addr)
 
                 while True:
                     data = client_socket.recv(1024)
@@ -29,12 +33,14 @@ def create_tcp_server():
 
                     # Assuming DF703.parse_data_DF703 can handle binary data directly
                     decoded_data = DF703.parse_data_DF703(data)
-                    
+                    logging.debug('Received data: %s', data)
+                    logging.debug('Decoded data: %s', decoded_data)
+
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logging.error("An error occurred: %s", e)
         finally:
-            if server_socket:  # Check if server_socket is not None before closing
-                server_socket.close()  # Close the server socket before restarting
+            if server_socket:
+                server_socket.close()
             time.sleep(1)
 
 if __name__ == '__main__':
