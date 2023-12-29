ce40b8455a38538f7c070617c2c919f941fc4033
Payam Amerian <payamamerian@gmail.com>
2023-12-30 07:33:27 +1000

    s

--- Node-Red/~/settings.js
+++ Node-Red/~/settings.js
@@ -1,5 +1,8 @@
-node-red admin hash-pw
-$2b$08$uZatqaR3qUzk6/QXGqIgrOTQi3kPgxgVV8bjaxUEAFmgtgT0cLYUm
+admin
+Elaboard1133
+
+node-red admin hash-pw  // Password for setting.js 
+
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install 18
