c6b0bab4baed5aa9492636f7f00b2331bbd3d66b
payamamerian <payamamerian@gmail.com>
2024-01-11 13:31:11 +1000

    Update Commands.sh

--- openRemote/Commands.sh
+++ openRemote/Commands.sh
@@ -1,2 +1,2 @@
-OR_HOSTNAME=185.167.98.198 OR_SSL_PORT=443 docker-compose -p openremote up -d
-OR_HOSTNAME=185.167.98.198 OR_PORT=8000 docker-compose -p openremote up -d
+OR_HOSTNAME=5.180.181.168 OR_SSL_PORT=443 docker-compose -p openremote up -d
+OR_HOSTNAME=5.180.181.168 OR_PORT=8000 docker-compose -p openremote up -d
